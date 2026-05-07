6e8b04380d22e526b9e6ab06250a098ab31c105c
YaXioz <alfianzyh@gmail.com>
2026-05-07 09:59:53 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -171,6 +171,6 @@ function startLevel(roomId) {
 
 // ================= START SERVER =================
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
